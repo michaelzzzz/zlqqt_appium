c6030b3388dd5f0d8b316921b225269b5338afa1
michaelzzzz <njfuzhoumiao@163.com>
2019-04-03 16:51:16 +0800

    增加多并发

--- src/common/zlqqt_desired_caps.py
+++ src/common/zlqqt_desired_caps.py
@@ -1,4 +1,8 @@
 # coding:utf-8
+import multiprocessing
+import subprocess
+from time import ctime
+
 __author__ = "James"
 
 from appium import webdriver
@@ -36,3 +40,41 @@ def zlqqt_desired():
 
 if __name__ == '__main__':
     zlqqt_desired()
+# with open('./config/desired_caps.yaml', 'r') as file:
+#     data = yaml.load(file)
+#
+# devices_list = ['127.0.0.1:62001','127.0.0.1:62026']
+#
+# def zlqqt_desired(udid,port):
+#     desired_caps={}
+#     desired_caps['platformName']=data['platformName']
+#     desired_caps['platformVersion']=data['platformVersion']
+#     desired_caps['deviceName']=data['deviceName']
+#     desired_caps['udid']=udid
+#
+#     desired_caps['app']=data['app']
+#     desired_caps['appPackage']=data['appPackage']
+#     desired_caps['appActivity']=data['appActivity']
+#     desired_caps['noReset']=data['noReset']
+#
+#     print('appium port:%s start run %s at %s' %(port, udid, ctime()))
+#     logging.info('===================启动app，开始测试=======================')
+#     driver=webdriver.Remote('http://'+str(data['ip'])+':'+str(port)+'/wd/hub', desired_caps)
+#     #driver.implicitly_wait(5)
+#     return driver
+#
+# #构建desired进程租
+# desired_process=[]
+#
+# #加载desied进程
+# for i in range(len(devices_list)):
+#     port = 4723 + 2 * i
+#     desired=multiprocessing.Process(target=zlqqt_desired,args=(devices_list[i],port))
+#     desired_process.append(desired)
+#
+# if __name__ == '__main__':
+#     # 启动多设备执行测试
+#     for desired in desired_process:
+#         desired.start()
+#     for desired in desired_process:
+#         desired.join()
